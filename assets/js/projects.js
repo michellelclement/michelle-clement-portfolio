6a71f80537c08484d6774edb0320aa43a3088159
Michelle Clement <michellelclement@gmail.com>
2022-01-19 09:43:53 +0000

    Add jquery code for project image fade out on hover

---
+++ assets/js/projects.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+    
+    $(".card-img").hover(function() {
+        $(this).animate({opacity: 0.1});
+    }
+    );
+})
+
+
+//
+//$(document).ready(function(){
+//    $(".card-img").hover(
+//        function(){$(".card-img").css('visibility', 'hidden');},
+//        function(){$(".button-projects").css('visibility', 'visible');},
+//    );
+//})
